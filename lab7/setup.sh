8df771c182a9e8321c248ed10f27b2f954ff5272
rn7cvj-dvfu <rn7cvj-student@outlook.com>
2026-02-01 02:08:28 +1000

    update lab7

--- lab7/setup.sh
+++ lab7/setup.sh
@@ -20,7 +20,7 @@ sudo bash -c "cat > /etc/lightdm/lightdm.conf" <<EOF
 [Seat:*]
 autologin-user=$KIOSK_USER
 autologin-user-timeout=5
-user-session=ubuntu
+user-session=xsession
 EOF
 
 sudo bash -c "cat > /usr/share/xsessions/xsession.desktop" <<EOF
@@ -44,6 +44,23 @@ $APP_PATH >> /tmp/kiosk.log 2>&1
 echo "start_apps.sh finished" >> /tmp/kiosk.log
 exec bash  # Держать сессию открытой
 EOF
+sudo chown $KIOSK_USER:$KIOSK_USER $KIOSK_HOME/.xsession
+sudo chmod +x $KIOSK_HOME/.xsession
+
+# === 4. Блокировка клавиш ===
+sudo bash -c "cat > $KIOSK_HOME/.Xmodmap" <<EOF
+keycode 67 = NoSymbol
+keycode 68 = NoSymbol
+keycode 69 = NoSymbol
+keycode 70 = NoSymbol
+keycode 71 = NoSymbol
+keycode 72 = NoSymbol
+keycode 73 = NoSymbol
+keycode 74 = NoSymbol
+keycode 75 = NoSymbol
+keycode 76 = NoSymbol
+EOF
+sudo chown $KIOSK_USER:$KIOSK_USER $KIOSK_HOME/.Xmodmap
 
 # === 5. Отключить доступ к терминалу (tty) ===
 sudo sed -i 's/^NAutoVTs=.*/NAutoVTs=0/' /etc/systemd/logind.conf || echo "NAutoVTs=0" | sudo tee -a /etc/systemd/logind.conf
